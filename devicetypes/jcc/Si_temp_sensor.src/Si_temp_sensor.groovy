20748b1c2b082fe7ec175efbc3ec0bc9e534f917
Cloute-Cazalaa <cazalaa@users.noreply.github.com>
2019-06-18 17:18:30 +0200

    Update Si_temp_sensor.groovy

--- devicetypes/jcc/Si_temp_sensor.src/Si_temp_sensor.groovy
+++ devicetypes/jcc/Si_temp_sensor.src/Si_temp_sensor.groovy
@@ -40,8 +40,8 @@ metadata {
 	attribute "currentDay", "String"
 	attribute "batteryRuntime", "String"
 
-	//fingerprint profileId: "0104", deviceId: "5F01", inClusters: "0000, 0003, FFFF, 0402, 0403, 0405", outClusters: "0000, 0004, FFFF", manufacturer: "LUMI", model: "lumi.weather", deviceJoinName: "Xiaomi Aqara Temp Sensor"
-	fingerprint profileId: "C00F", deviceId: "0302", inClusters: "0000, 0003, FFFF, 0402", outClusters: "0000, FFFF", manufacturer: "Ember", model: "Ember.weather", deviceJoinName: "My Temp Sensor"
+	fingerprint profileId: "0104", deviceId: "5F01", inClusters: "0000, 0003, FFFF, 0402, 0403, 0405", outClusters: "0000, 0004, FFFF", manufacturer: "LUMI", model: "lumi.weather", deviceJoinName: "Xiaomi Aqara Temp Sensor"
+	//fingerprint profileId: "C00F", deviceId: "0302", inClusters: "0000, 0003, FFFF, 0402", outClusters: "0000, FFFF", manufacturer: "Ember", model: "Ember.weather", deviceJoinName: "My Temp Sensor"
 
 	command "resetBatteryRuntime"
 	}
